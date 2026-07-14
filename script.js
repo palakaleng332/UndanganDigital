9a564ecd254dc1321be261e3266f7884581c5a68
Soehendra <palakaleng332@gmail.com>
2026-07-14 21:46:04 +0700

    Update wedding invitation

--- script.js
+++ script.js
@@ -614,3 +614,40 @@ window.addEventListener("load",()=>{
     }
 
 });
+
+/* ======================================
+   MOBILE NAV ACTIVE
+====================================== */
+
+const sections=document.querySelectorAll("section");
+const navLinks=document.querySelectorAll(".mobile-nav a");
+
+window.addEventListener("scroll",()=>{
+
+let current="";
+
+sections.forEach(section=>{
+
+const top=section.offsetTop-180;
+
+if(scrollY>=top){
+
+current=section.getAttribute("id");
+
+}
+
+});
+
+navLinks.forEach(link=>{
+
+link.classList.remove("active");
+
+if(link.getAttribute("href")==="#"+current){
+
+link.classList.add("active");
+
+}
+
+});
+
+});
